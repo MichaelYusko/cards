ccedf33571c62b0317b26f83e96c974253e5a6e4
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-02 23:46:59 +0300

    Add deal function

--- cards/deck.go
+++ cards/deck.go
@@ -4,12 +4,14 @@ import "fmt"
 
 type deck []string
 
+// Print all cards
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
+// Create an array with cards
 func newDeck() deck {
 	cards := deck{}
 
@@ -22,4 +24,9 @@ func newDeck() deck {
 		}
 	}
 	return cards
+}
+
+// Deal the cards
+func deal(d deck, handSize int) (deck, deck){
+	return d[:handSize], d[handSize:]
 }
