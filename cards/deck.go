f94ece0dcfdb0f8d998e96b5459f8cd9cb2cbff4
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-03 22:39:43 +0300

    Add saveToFile function

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
 
 type deck []string
 
@@ -27,6 +31,16 @@ func newDeck() deck {
 }
 
 // Deal the cards
-func deal(d deck, handSize int) (deck, deck){
+func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// Join by comma
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// Save to file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
