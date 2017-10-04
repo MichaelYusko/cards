74be670df542f0f82390bc4a7671730b6367d291
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-04 20:46:03 +0300

    add test for newDeckFromFile function

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -19,4 +22,27 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected first card of 'Four of Clubs' but got %v", d[len(d) - 1])
 	}
+}
+
+
+func TestSaveToDeckFromFile(t *testing.T) {
+
+	// Remove the file if exists
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+
+	// Save into file
+	deck.saveToFile("_decktesting")
+
+	// Crete new deck from the file
+	loadDeck := newDeckFromFile("_decktesting")
+
+	// Check if length of the array greater than 16
+	if len(loadDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadDeck))
+	}
+
+	// Remove the file after testing
+	os.Remove("_decktesting")
 }
